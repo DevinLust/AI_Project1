d42bfabe2ae821859d549d491f1d28ef370503e2
jacobbailey8 <jbaileydsi87@gmail.com>
2024-01-30 17:07:48 -0500

    added times dictionary

--- UCSearch.py
+++ UCSearch.py
@@ -2,7 +2,9 @@
 import heapq
 
 GOAL_STATE = [True, True, True, True, True]  # A, B, C, D, P
-POWER_INDEX = 4 # Index of the power supply
+POWER_INDEX = 4  # Index of the power supply
+
+TIMES = {"0": 1, "1": 2, "2": 5, "3": 10}
 
 
 class Node:
@@ -19,27 +21,31 @@ class Node:
         return "state: " + str(self.state) + ", cost: " + str(self.cost)
 
 # return the index of a node that has the same state as child, -1 otherwise
+
+
 def repeatState(heap, child):
     for i in range(len(heap)):
         if heap[i].state == child.state:
             return i
     return -1
 
+
 def UniformCost(initialState, goalState):
     node = Node(state=initialState, parent=None, cost=0)
     explored = []
     heap = []
     heapq.heappush(heap, node)
-    print("Heap: " + str(heap)) # debug print
+    print("Heap: " + str(heap))  # debug print
     while (len(heap) > 0):
-        smallest_item = heapq.heappop(heap) # the heap stores nodes
-        print("Expanded Node: " + smallest_item.to_String()) # debug print
+        smallest_item = heapq.heappop(heap)  # the heap stores nodes
+        print("Expanded Node: " + smallest_item.to_String())  # debug print
         if (smallest_item.state == goalState):
             return smallest_item
         explored.append(smallest_item)
         successors = succ(smallest_item.state)
         for nextState in successors:
-            child = Node(state=nextState, parent=smallest_item, cost=smallest_item.cost + 1)
+            child = Node(state=nextState, parent=smallest_item,
+                         cost=smallest_item.cost + 1)
             repeatIdx = repeatState(heap, child)
             if (not (repeatIdx != -1 or child in explored)):
                 heapq.heappush(heap, child)
@@ -68,7 +74,7 @@ def succ(initialState):
                 if (initialState[j] == powerLocation):
                     # handles the cases in which pairs cross
                     potentialState = initialState.copy()
-                    potentialState[i] = not potentialState[i] 
+                    potentialState[i] = not potentialState[i]
                     potentialState[j] = not potentialState[j]
                     potentialState[POWER_INDEX] = not potentialState[POWER_INDEX]
                     result.append(potentialState)
